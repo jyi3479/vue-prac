7a501290fe768c12202116b368eb68a8a67db061
jyi3479 <jyi3479@naver.com>
2022-05-09 00:46:21 +0900

    form project

---
+++ learn-vue-js-master/vue-form/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <label for="username">id: </label>
+      <input id="username" type="text" v-model="username" />
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input id="password" type="password" v-model="password" />
+    </div>
+    <button type="submit">login</button>
+  </form>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    submitForm: function () {
+      // form의 새로고침을 막아준다.
+      //event.preventDefault(); 대신 submit.prevent
+      console.log(this.username, this.password);
+      var url = "https://jsonplaceholder.typicode.com/users";
+      var data = {
+        username: this.username,
+        password: this.password,
+      };
+      axios
+        .post(url, data)
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
+
+<style></style>
